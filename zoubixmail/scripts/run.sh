d3c910377ed5d29094ccd37f27376a97a718de64
Fred Lehmann <fred@zoubix.net>
2020-04-20 12:14:35 +0000

    add some chown to run.sh

--- zoubixmail/scripts/run.sh
+++ zoubixmail/scripts/run.sh
@@ -3,8 +3,12 @@ set -e # exit on error
 
 # Launch
 rm -f /var/spool/postfix/pid/*.pid
-/usr/sbin/postmap /etc/postfix/local_recipient_map
-/usr/sbin/postalias /etc/postfix/aliases
+chown -R bernard:bernard /home/bernard
+chown -R fred:fred /home/fred
+chown -R cel:cel /home/cel
+chown -R klara:klara /home/klara
 chown -R postfix:postfix /var/spool/postfix
 chown -R _rspamd:_rspamd /var/lib/rspamd
+/usr/sbin/postmap /etc/postfix/local_recipient_map
+/usr/sbin/postalias /etc/postfix/aliases
 exec /usr/bin/supervisord -n
